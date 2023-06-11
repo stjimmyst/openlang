eeb651b8ec414f68caf4ca2dbf729289410ea629
Dmitry Turchenkov <dmitry@wgames.com>
2023-06-11 14:33:09 -0400

    first commit

--- server/main.py
+++ server/main.py
@@ -3,11 +3,14 @@ import time, os
 
 app = Flask(__name__)
 
-
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
+@app.route('/')
+def default_route():
+    return {'value':"Hello pridurok"}
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port,ssl_context='adhoc')
+    app.run(debug=True, host='0.0.0.0', port=port)
