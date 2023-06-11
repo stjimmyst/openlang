c0f6f8a65929806e21c9234fb5b7a9a8a0baa802
Dmitry Turchenkov <dmitry@wgames.com>
2023-06-11 19:53:03 -0400

    added mysql

--- server/main.py
+++ server/main.py
@@ -1,11 +1,31 @@
 from flask import Flask
 import time, os
+import mysql.connector
 
 app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+  host="35.184.19.133",
+  user="root",
+  password="Flamingt0rch",
+  database="data"
+)
+
+def getRandomTopic():
+    mycursor = mydb.cursor(dictionary=True)
+    mycursor.execute("SELECT topic FROM Topics ORDER BY RAND() LIMIT 1")
+    myresult = mycursor.fetchone()
+    return myresult["topic"]
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/getRandomTopic')
+def route_getRandomTopic():
+    res = getRandomTopic()
+    return {'topic': res}
 
 @app.route('/')
 def default_route():
