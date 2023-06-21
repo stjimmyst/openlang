a3a213e0bb1f647dd44eb95dd824cbc4a7f1de38
Dmitry Turchenkov <dmitry@wgames.com>
2023-06-21 17:33:38 -0400

    open routes

--- server/gpt.py
+++ server/gpt.py
@@ -2,7 +2,7 @@ import openai
 import os
 import json
 
-API_KEY = "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"
+API_KEY = "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"
 prompt_template = """analyse my IELTS writing task1 by 4 criterias with score in range 1 to 9. Your response should be in json format {format}. Per each critera provide:
 comment: detalaied answer and expalanation
 improvements: how can  improve this criteria?
@@ -43,8 +43,8 @@ Alexey Ivanov.
 format="""{criterias:[{comment, improvement, error, band}]}"""
 
 def estimate_text(question, answer):
-    os.environ["OPENAI_API_KEY"] = API_KEY
-    openai.api_key = os.environ["OPENAI_API_KEY"]
+    #os.environ["OPENAI_API_KEY"] = API_KEY
+    openai.api_key = "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"
     prompt = prompt_template.format(task=task,response=response,format=format)
     print(prompt)
     completion = openai.ChatCompletion.create(
@@ -57,3 +57,5 @@ def estimate_text(question, answer):
     tmp = completion.choices[0].message["content"]
     print(tmp)
     return tmp
+
+#estimate_text("v","v")
--- server/main.py
+++ server/main.py
@@ -37,45 +37,45 @@ test_resp = """
 }
 }
 """
-API_KEY = "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"
-prompt_template = """analyse my IELTS writing task1 by 4 criterias with score in range 1 to 9. Your response should be in json format {format}. Per each critera provide:
-comment: detalaied answer and expalanation
-improvements: how can  improve this criteria?
-errors: example of errors in essay.
-band: you estimated band by criteria
-
-TASK:
-{task}
-RESPONSE:
-{response}
-"""
-task = """
-You recently visited a dog park and were disturbed by the lack of control the owners
-imposed on their dogs, making them dangerous for everyone around.
-
-Write an email to your local municipality in about 150-200
-words. Your email should include the following things:
-• Describe the situation
-• Explain why the dogs were a danger to others
-• Propose a solution
-"""
-response = """
-Dear local municipality,
-The main purpose of my writing today is to lodge a complaint about the dog park in our town, that I visited with my family last weekend.
-To begin with, I was playing with my kids when a gigantic dog without a leash
-suddenly appeared behind my back. 
-My kids were very scared by this aggressive barking creature and started crying.
-It's very important to respect the rule of each and other in our society. 
-Big creatures like this dog, walking around the park withut control, may be not only a source of danger but also a cause of various viruses for humans.
-Based on what I have provided you today, I may suggest a few options to solve this problem. 
-Firstly, I would strongly recommend increasing the amount of penalty for breaking the rules in our park. Furthermore, it will be a good idea to move the dog park far away from the children's playground.
-I am looking forward to seeing how my suggestions translate to prompt actions from your side.
-Your kind cooperation would be most appreciated.
-Sincerely,
-Alexey Ivanov.
-"""
-
-format="""{criterias:[{comment, improvement, error, band}]}"""
+# API_KEY = "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"
+# prompt_template = """analyse my IELTS writing task1 by 4 criterias with score in range 1 to 9. Your response should be in json format {format}. Per each critera provide:
+# comment: detalaied answer and expalanation
+# improvements: how can  improve this criteria?
+# errors: example of errors in essay.
+# band: you estimated band by criteria
+#
+# TASK:
+# {task}
+# RESPONSE:
+# {response}
+# """
+# task = """
+# You recently visited a dog park and were disturbed by the lack of control the owners
+# imposed on their dogs, making them dangerous for everyone around.
+#
+# Write an email to your local municipality in about 150-200
+# words. Your email should include the following things:
+# • Describe the situation
+# • Explain why the dogs were a danger to others
+# • Propose a solution
+# """
+# response = """
+# Dear local municipality,
+# The main purpose of my writing today is to lodge a complaint about the dog park in our town, that I visited with my family last weekend.
+# To begin with, I was playing with my kids when a gigantic dog without a leash
+# suddenly appeared behind my back.
+# My kids were very scared by this aggressive barking creature and started crying.
+# It's very important to respect the rule of each and other in our society.
+# Big creatures like this dog, walking around the park withut control, may be not only a source of danger but also a cause of various viruses for humans.
+# Based on what I have provided you today, I may suggest a few options to solve this problem.
+# Firstly, I would strongly recommend increasing the amount of penalty for breaking the rules in our park. Furthermore, it will be a good idea to move the dog park far away from the children's playground.
+# I am looking forward to seeing how my suggestions translate to prompt actions from your side.
+# Your kind cooperation would be most appreciated.
+# Sincerely,
+# Alexey Ivanov.
+# """
+#
+# format="""{criterias:[{comment, improvement, error, band}]}"""
 
 app = Flask(__name__)
 
@@ -91,8 +91,7 @@ def getRandomTopic():
     # mycursor.execute("SELECT topic FROM Topics ORDER BY RAND() LIMIT 1")
     # myresult = mycursor.fetchone()
     # return myresult["topic"]
-    text = """
-    An English speaking friend wants to spend a two week holiday in your region and has written
+    text = """An English speaking friend wants to spend a two week holiday in your region and has written
 asking for information and advice. Write a letter to your friend, in your letter:
 • Offer advice about where to stay
 • Give her advice about what to do
